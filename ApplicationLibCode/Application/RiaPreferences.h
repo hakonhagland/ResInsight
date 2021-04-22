62844e4d881a734634f703f7857c913da1f47e71
jonjenssen <jon@soundsoft.no>
2021-04-22 16:19:41 +0200

    More functionality - load grid from preprocessing, update fault collections to show fra settings, initialize with user settings. Start adding processing commands. Still work in progress

--- ApplicationLibCode/Application/RiaPreferences.h
+++ ApplicationLibCode/Application/RiaPreferences.h
@@ -150,7 +150,8 @@ public:
     QString geomechFRAPreprocCommand() const;
     QString geomechFRAPostprocCommand() const;
     QString geomechFRAMacrisCommand() const;
-    QString geomechFRADefaultXML() const;
+    QString geomechFRADefaultBasicXML() const;
+    QString geomechFRADefaultAdvXML() const;
 
     // Summary readers
     SummaryReaderMode summaryDataReader() const;
@@ -268,7 +269,8 @@ private:
     caf::PdmField<QString> m_geomechFRAPreprocCommand;
     caf::PdmField<QString> m_geomechFRAPostprocCommand;
     caf::PdmField<QString> m_geomechFRAMacrisCommand;
-    caf::PdmField<QString> m_geomechFRADefaultXML;
+    caf::PdmField<QString> m_geomechFRADefaultBasicXML;
+    caf::PdmField<QString> m_geomechFRADefaultAdvXML;
 
     // Summary data
     caf::PdmField<bool> m_createOptimizedSummaryDataFile;
